1b211f54855421e40900f54b8e34964b5a7e8b68
Lily <yyn605@outlook.com>
2020-06-11 17:51:06 +0800

    add interface implementation of MCPosition

---
+++ MarsExplorer/MarsCar/MCPosition.h
@@ -0,0 +1,30 @@
+//
+//  MCPosition.h
+//  MarsExplorer
+//
+//  Created by 宁 on 2020/6/11.
+//  Copyright © 2020 宁. All rights reserved.
+//
+
+typedef NS_ENUM(NSUInteger, MCOrientation) {
+    MCORIENTATION_NORTH,
+    MCORIENTATION_SOUTH,
+    MCORIENTATION_WEST,
+    MCORIENTATION_EAST
+};
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MCPosition : NSObject
+
+@property (nonatomic, assign) NSUInteger x;
+
+@property (nonatomic, assign) NSUInteger y;
+
+@property (nonatomic, assign) MCOrientation orientation;
+
+- (instancetype)initWithPoint:(CGPoint)point orientation:(MCOrientation)orientation;
+
+@end
+
+NS_ASSUME_NONNULL_END
