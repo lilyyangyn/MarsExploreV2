4f85598499e713ee18258fde47ed61b6a2980b65
Lily <yyn605@outlook.com>
2020-06-11 18:41:44 +0800

    add implementation of MCCar & MCPosition

---
+++ MarsExplorer/MarsCar/MCCar.m
@@ -0,0 +1,15 @@
+//
+//  MCCar.m
+//  MarsExplorer
+//
+//  Created by 宁 on 2020/6/11.
+//  Copyright © 2020 宁. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@implementation MCCar
+
+<#methods#>
+
+@end
