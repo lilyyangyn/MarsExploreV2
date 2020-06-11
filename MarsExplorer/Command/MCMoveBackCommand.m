f5b5336e241bf64c3f90c23aabb66398e54fd58e
Lily <yyn605@outlook.com>
2020-06-11 19:18:32 +0800

    implement MCMoveCommand classes

---
+++ MarsExplorer/Command/MCMoveBackCommand.m
@@ -0,0 +1,29 @@
+//
+//  MCMoveBackCommand.m
+//  MarsExplorer
+//
+//  Created by 宁 on 2020/6/11.
+//  Copyright © 2020 宁. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "MCMoveFrontCommand.h"
+#import "MCPositionDefine.h"
+
+@implementation MCMoveFrontCommand
+
+- (MCPositionDefine *)excuteWithCurrentPosition:(MCPositionDefine *)position
+{
+    if (position.orientation == MCORIENTATION_EAST) {
+        position.x -= self.step;
+    } else if (position.orientation == MCORIENTATION_WEST){
+        position.x += self.step;
+    } else if (position.orientation == MCORIENTATION_NORTH){
+        position.y -= self.step;
+    } else if (position.orientation == MCORIENTATION_SOUTH){
+        position.x += self.step;
+    }
+    return position;
+}
+
+@end
