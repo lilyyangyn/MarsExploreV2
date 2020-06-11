c43ca5a291ec6d2bce8e76a4e036221d38561182
Lily <yyn605@outlook.com>
2020-06-11 17:44:13 +0800

    add interface of MCCar

---
+++ MarsExplorer/MarsCar/MCCar.h
@@ -0,0 +1,15 @@
+//
+//  MCCar.h
+//  MarsExplorer
+//
+//  Created by 宁 on 2020/6/11.
+//  Copyright © 2020 宁. All rights reserved.
+//
+
+@class MCPosition;
+
+NS_ASSUME_NONNULL_BEGIN
+@interface MCCar : NSObject
+@property (nonatomic, strong) MCPosition *postion;
+
+@end
